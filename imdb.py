0262ccc3f4af9b0ff7e9065cc5d34ea80190004a
schurmann <jonathan.schurmann@gmail.com>
2018-02-20 19:57:51 +0100

    Added log config

--- imdb.py
+++ imdb.py
@@ -1,8 +1,17 @@
+import logging
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
 import google_sheets
 
 if __name__ == '__main__':
+    configure_logging(install_root_handler=False)
+    logging.basicConfig(
+        filename='/tmp/scraper.log',
+        format='%(levelname)s: %(message)s',
+        level=logging.INFO
+    )
     process = CrawlerProcess(get_project_settings())
     process.crawl('ratings')
     process.start()
